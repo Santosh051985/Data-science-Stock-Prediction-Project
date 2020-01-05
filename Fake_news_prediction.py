36cb97c352be25f9662ef6f12ebb618279c3a4c8
Santosh Kumar <dssant@gmail.com>
2020-01-06 02:11:36 +0530

    Update Fake_news_prediction.py

--- Fake_news_prediction.py
+++ Fake_news_prediction.py
@@ -14,13 +14,19 @@ from sklearn.metrics import accuracy_score, confusion_matrix
  labels=df.label
  labels.head()
  labels.tail()
-          #DataFlair - Split the dataset
+          # Split the dataset
  x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
-       #DataFlair - Initialize a TfidfVectorizer 
+       #Initialize a TfidfVectorizer 
 tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
-    #DataFlair - Fit and transform train set, transform test set
-    tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
+    # Fit and transform train set, transform test set
+tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
 tfidf_test=tfidf_vectorizer.transform(x_test)
-#DataFlair - Initialize a PassiveAggressiveClassifier
+#  Initialize a PassiveAggressiveClassifier
  pac= PassiveAggressiveClassifier(max_iter=50)
  pac.fit(tfidf_train,y_train)
+# Predict on the test set and calculate accuracy
+ y_pred=pac.predict(tfidf_test)
+ score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+#  Build confusion matrix
+    confusion_matrix(y_test,y_pred, labels=['FAKE','REAL'])
